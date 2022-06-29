175234f98a2b345294612d40bc410a9befac3051
JulietaEsains <jesains@gmail.com>
2022-06-29 03:34:59 -0300

    Atributo borrado del modelo de Usuario

--- db/migrate/20220622095019_create_users.rb
+++ db/migrate/20220622095019_create_users.rb
@@ -5,7 +5,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :username
       t.string :email
       t.string :password_digest
-      t.string :picture_url
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_24_123929) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
-    t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
